26bb795ffd16cbc6a2f59f3015938ecdd49c5baa
“Tyler <tylerpalef@gmail.com>
2018-10-02 16:57:46 -0400

    added user controller for easier session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
-end
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+   def current_user
+     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+   end
+   helper_method :current_user
+
+   def require_login
+     if !current_user
+       flash[:notice] = "You must be logged in!"
+       redirect_to 'new'
+     end
+   end
+ end
